df0a7b1004a15b39184a1d9fa9c7e374971972e5
Loc Mai <locmai0201@gmail.com>
2021-04-30 22:47:43 +0700

    add default apps role

--- apps/terraform/vault.tf
+++ apps/terraform/vault.tf
@@ -8,3 +8,29 @@ resource "vault_kubernetes_auth_backend_config" "default_kubernetes_auth_backend
   kubernetes_ca_cert     = data.kubernetes_secret.vault_vault_token.data["ca.crt"]
   token_reviewer_jwt     = data.kubernetes_secret.vault_vault_token.data["token"]
 }
+
+resource "vault_policy" "apps_all" {
+  name = "apps_all"
+
+  policy = <<EOT
+path "*" {
+  capabilities = [ "create", "update", "read", "delete", "list" ]
+}
+EOT
+}
+
+resource "vault_kubernetes_auth_backend_role" "apps_all" {
+  backend                          = vault_auth_backend.kubernetes.path
+  role_name                        = "apps"
+  bound_service_account_names      = ["apps"]
+  bound_service_account_namespaces = [var.default_namespace]
+  token_ttl                        = 3600
+  token_policies                   = ["apps_all"]
+}
+
+resource "kubernetes_service_account" "apps" {
+    metadata {
+        name      = "apps"
+        namespace = "apps"
+    }
+}
