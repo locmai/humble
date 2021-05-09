39a7bbc1bb9b67891c4196918dadd5b9db37b704
Loc Mai <locmai0201@gmail.com>
2021-05-09 17:13:26 +0700

    update identity schema

---
+++ apps/argocd/apps/scripts/create_identity.sh
@@ -0,0 +1,20 @@
+curl --request POST -sL \
+    --header "Content-Type: application/json" \
+    --request POST \
+    --data '{
+  "schema_id": "default",
+  "traits": {
+    "email": "locmai0201@gmail.com",
+    "name": "Loc Mai"
+  }
+}' \
+    http://localhost:4434/identities
+
+curl --request POST -sL \
+    --header "Content-Type: application/json" \
+    --request POST \
+    --data '{
+  "expires_in": "12h",
+  "identity_id": "83959fbc-f7a2-4a30-a76f-1737c69b366d"
+}' \
+    http://localhost:4434/recovery/link
