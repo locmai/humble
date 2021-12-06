9db07e8ba259495851ae02a807eb8453bc37f0da
Loc Mai <locmai0201@gmail.com>
2021-12-06 11:49:44 +0700

    chores (apps): refactor hierachy of apps structure

--- infras/terraform/locals.tf
+++
@@ -1,8 +0,0 @@
-locals {
-  nodes    = yamldecode(file("hosts.yaml"))["all"]["children"]["nodes"]["hosts"]
-
-  ssh_user = "locmai"
-  ssh_key  = file("~/.ssh/id_rsa_lmlabs")
-
-  monitoring_namespace = "monitoring"
-}
