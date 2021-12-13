26e52d1baf935c6f96958d82aef40b79ac840a95
Loc Mai <locmai0201@gmail.com>
2021-12-13 15:15:41 +0700

    chore(global): remove pulumi cuz it sucks af

--- global/__main__.py
+++
@@ -1,5 +0,0 @@
-from cloudflare import create_cloudflare_resources
-from kubernetes import create_kubernetes_resources
-
-create_cloudflare_resources()
-# create_kubernetes_resources()
--- global/secrets.py
+++
@@ -1,5 +0,0 @@
-# pulumi config set cloudflare:tunnel_secret --secret
-
-from secrets import token_urlsafe
-
-print(token_urlsafe(64))
