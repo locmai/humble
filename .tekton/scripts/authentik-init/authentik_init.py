8d7fa433c08f3a9e9e301a05df93cea5973398a5
Loc Mai <locmai0201@gmail.com>
2021-12-25 03:21:19 +0700

    chore: almost done

--- .tekton/scripts/authentik-init/authentik_init.py
+++ .tekton/scripts/authentik-init/authentik_init.py
@@ -1,39 +1,77 @@
-base_url = "https://authentik.maibaloc.com/api/v3"
-api_oauth2_flow = f"{base_url}/instances/?designation=authorization&ordering=slug"
-api_oauth2 = f"{base_url}/providers/oauth2/"
-test_api =f"{base_url}/flows/bindings/"
-payload = {
-    'name': "gitea",
-    'authorization_flow': None,
-    'client_type':  "confidential",
-    'client_id': None, 
-    'client_secret': None,
-    'access_code_validity': 'minutes=1',
-    'token_validity': 'days=30',
-}
+import base64, sys, requests, json
 
 from kubernetes import client, config
-import base64
-import sys
+
+
+base_url = "https://authentik.maibaloc.com/api/v3"
+
+# Init kubernetes client
 config.load_kube_config(config_file='../../../metal/kubeconfig.yaml')
-
 v1 = client.CoreV1Api()
-ak_admin_token_encoded = v1.read_namespaced_secret("authentik-aka-secret", "platform").data['ak_admin_token']
 
-ak_admin_token = (base64.b64decode(ak_admin_token_encoded)).decode()
+# Get authentik secret
+def decode(encoded_secret: str) -> str:
+    return (base64.b64decode(encoded_secret)).decode()
 
-print(ak_admin_token)
+ak_secrets = v1.read_namespaced_secret("authentik-aka-secret", "platform").data
 
-import requests
+ak_admin_token = decode(ak_secrets['ak_admin_token'])
 
-res = requests.get(test_api,headers={'Authorization': f"Bearer {ak_admin_token}"})
+gitea_oauth2_client_id = decode(ak_secrets['ak_gitea_oauth2_client_id'])
 
-print(res.status_code)
-
-print(res.json())
-
-# Kubernetes client
-
-# Get Token from secret
+gitea_oauth2_client_secret = decode(ak_secrets['ak_gitea_oauth2_client_secret'])
 
 # Get the default-provider-authorization-explicit-consent flow from /flows/bindings/
+flow_binding_api_url =f"{base_url}/flows/bindings"
+res = requests.get(flow_binding_api_url,headers={'Authorization': f"Bearer {ak_admin_token}"})
+resp_json = res.json()
+target_auth_flow = [
+    flow for flow in resp_json["results"] if flow["stage_obj"]["name"] == "default-provider-authorization-consent"
+]
+
+# Get rsa_key id via /crypto/certificatekeypairs
+crypto_cert_api_url = f"{base_url}/crypto/certificatekeypairs"
+res = requests.get(crypto_cert_api_url,headers={'Authorization': f"Bearer {ak_admin_token}"})
+resp_json = res.json()
+self_signed_cert_id = [
+    cert for cert in resp_json["results"] if cert["name"] == "authentik Self-signed Certificate"
+]
+
+
+# Get default OpenID/OAuth2 property mappings /propertymappings/all/ [email, openid, profile] 
+prop_mapping_api_url = f"{base_url}/propertymappings/all"
+res = requests.get(prop_mapping_api_url,headers={'Authorization': f"Bearer {ak_admin_token}"})
+resp_json = res.json()
+props_mapping = [
+    prop["pk"] for prop in resp_json["results"] if "authentik default OAuth Mapping: OpenID" in prop["name"] 
+]
+
+payload = {
+    'name': "gitea",
+    'authorization_flow': target_auth_flow[0]["target"],
+    'property_mappings': props_mapping,
+    'client_type':  "confidential",
+    'client_id': gitea_oauth2_client_id, 
+    'client_secret': gitea_oauth2_client_secret,
+    'access_code_validity': 'minutes=1',
+    'token_validity': 'days=30',
+    'include_claims_in_id_token': True,
+    'jwt_alg': 'RS256',
+    'rsa_key': self_signed_cert_id[0]["pk"],
+    "redirect_uris": "",
+    "sub_mode": "hashed_user_id",
+    "issuer_mode": "per_provider"
+}
+
+
+# Create provider via /providers/oauth2
+provider_oauth2_api_url = f"{base_url}/providers/oauth2"
+
+res = requests.post(provider_oauth2_api_url,
+    headers={'Authorization': f"Bearer {ak_admin_token}"},
+    json=payload,
+    allow_redirects=False)
+
+# res = requests.get(provider_oauth2_api_url, headers={'Authorization': f"Bearer {ak_admin_token}"})
+
+print(res.status_code)
