896d0123bc3836fd4eec153acc8ca13b70f04ac9
Loc Mai <locmai0201@gmail.com>
2021-12-31 22:55:48 +0700

    chore: set sync-wave for all apps

--- global/main.tf
+++ global/main.tf
@@ -19,7 +19,24 @@ resource "cloudflare_record" "tunnel" {
   ttl     = 1 # Auto
 }
 
+
+resource "kubernetes_namespace" "namespaces" {
+  for_each = toset([
+    "cert-manager",
+    "cloudflared",
+    "external-dns",
+  ])
+
+  metadata {
+    name = each.key
+  }
+}
+
 resource "kubernetes_secret" "cloudflared_credentials" {
+  depends_on = [
+    kubernetes_namespace.namespaces
+  ]
+
   metadata {
     name = "cloudflared-credentials"
     namespace = "cloudflared"
@@ -62,6 +79,10 @@ resource "cloudflare_api_token" "external_dns" {
 }
 
 resource "kubernetes_secret" "external_dns_token" {
+  depends_on = [
+    kubernetes_namespace.namespaces
+  ]
+
   metadata {
     name = "cloudflare-api-token"
     namespace = "external-dns"
@@ -99,6 +120,10 @@ resource "cloudflare_api_token" "cert_manager" {
 }
 
 resource "kubernetes_secret" "cert_manager_token" {
+  depends_on = [
+    kubernetes_namespace.namespaces
+  ]
+
   metadata {
     name = "cloudflare-api-token"
     namespace = "cert-manager"
--- global/versions.tf
+++ global/versions.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "~> 1.1.0"
+  required_version = "~> 1.0.11"
 
   backend "remote" {
     hostname     = "app.terraform.io"
