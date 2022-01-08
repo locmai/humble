18c2ab9fe992b4859c74b1eabe8a93080da68c52
Loc Mai <locmai0201@gmail.com>
2022-01-08 19:24:07 +0700

    doc: remove unused imports

--- docs/doczrc.js
+++ docs/doczrc.js
@@ -1,8 +1,4 @@
-import images from 'remark-images'
-import emoji from 'remark-emoji'
-
 export default {
     files: '**/*.{md,markdown,mdx}',
-    title: 'Humble documentation',
-    mdPlugins: [images, emoji],
+    title: 'Humble',
 }
