fa4038790fff0690300ab253238583a9204d6329
Loc Mai <locmai0201@gmail.com>
2022-09-09 12:40:09 +0700

    chore: remove more files

--- tools/alerttests/fire.sh
+++
@@ -1,8 +0,0 @@
-kubectl port-forward -n observability svc/monitoring-kube-prometheus-alertmanager 46833:9093
-
-amtool alert list
-
-amtool alert add "Test Alert" ops=yuta \
-        --annotation=runbook='http://runbook.biz' \
-        --annotation=summary='summary of the alert' \
-        --annotation=description='description of the alert'
