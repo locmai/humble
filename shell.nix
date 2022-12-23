29cb395381f56755917c234668a48c8ecbbdbdbc
Loc Mai <locmai0201@gmail.com>
2022-12-23 23:40:45 +0700

    bump k3s version and nix-shell

--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
 # https://status.nixos.org
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/f034b5693a26625f56068af983ed7727a60b5f8b.tar.gz") {} }:
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/refs/tags/22.11.tar.gz") {} }:
 
 let
   python-packages = pkgs.python3.withPackages (p: with p; [
@@ -8,6 +8,22 @@ let
     netaddr
     rich
   ]);
+  # cillium = pkgs.mkDerivation {
+  #   pname = "cillium";
+  #   version = "0.0.1";
+  #   buildInputs = with pkgs; [ curl ];
+  #   phases = [ "installPhase" ];
+  #   installPhase = ''
+  #     CILIUM_CLI_VERSION=$(curl -s https://raw.githubusercontent.com/cilium/cilium-cli/master/stable.txt)
+  #     CLI_ARCH=amd64
+
+  #     if [ "$(uname -m)" = "aarch64" ]; then CLI_ARCH=arm64; fi
+  #     curl -L --fail --remote-name-all https://github.com/cilium/cilium-cli/releases/download/${CILIUM_CLI_VERSION}/cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
+  #     sha256sum --check cilium-linux-${CLI_ARCH}.tar.gz.sha256sum
+  #     sudo tar xzvfC cilium-linux-${CLI_ARCH}.tar.gz /usr/local/bin
+  #     rm cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
+  #   '';
+  # };
 in
 pkgs.mkShell {
   buildInputs = with pkgs; [
